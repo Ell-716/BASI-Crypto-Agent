8c3b2ae568ddcccbe3128cb32b35b490762a0e2a
Ell-716 <elenabai.2021@gmail.com>
2025-03-27 16:32:14 +0300

    Create a route for fear & greed index

--- backend/app/routes/dashboard_routes.py
+++ backend/app/routes/dashboard_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from backend.app.dashboard.coin_data import get_live_top_10_coins
+from backend.app.dashboard.fear_greed import fetch_fear_and_greed_index
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
@@ -9,6 +10,11 @@ def dashboard_top_10_coins():
     data = get_live_top_10_coins()
     return jsonify(data)
 
-@dashboard_bp.route("/test", methods=["GET"])
-def test():
-    return jsonify({"message": "route works"})
+
+@dashboard_bp.route("/dashboard/fear-greed", methods=["GET"])
+def dashboard_fear_greed():
+    try:
+        data = fetch_fear_and_greed_index()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
