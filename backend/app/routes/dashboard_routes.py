1b5c9c6bd79220c43bae07bdb9f723fd029b83b4
Ell-716 <elenabai.2021@gmail.com>
2025-03-27 17:21:40 +0300

    Add top_volume route

--- backend/app/routes/dashboard_routes.py
+++ backend/app/routes/dashboard_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from backend.app.dashboard.coin_data import get_live_top_10_coins
 from backend.app.dashboard.fear_greed import fetch_fear_and_greed_index
+from backend.app.dashboard.top_volume import get_top_coin_by_24h_volume
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
@@ -16,5 +17,14 @@ def dashboard_fear_greed():
     try:
         data = fetch_fear_and_greed_index()
         return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@dashboard_bp.route("/dashboard/top-volume", methods=["GET"])
+def dashboard_top_volume():
+    try:
+        coin = get_top_coin_by_24h_volume()
+        return jsonify(coin)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
