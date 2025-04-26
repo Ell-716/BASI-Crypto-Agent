a83d48f059c25b276ce61f15acea329d644a9627
Ell-716 <elenabai.2021@gmail.com>
2025-04-26 23:47:23 +0200

    Push the footer to the very bottom of the screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import AIPredictions from './pages/AIPredictions';
 import About from './pages/About';
@@ -16,32 +17,37 @@ function PrivateRoute({ children }) {
 
 function AppContent() {
   const location = useLocation();
-  const hideNavbar = ['/signup', '/login', '/reset-password'].includes(location.pathname);
+  const hideNavbarAndFooter = ['/signup', '/login', '/reset-password'].includes(location.pathname);
 
   return (
     <>
-      {!hideNavbar && <Navbar />}
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
-        <Route path="/coin/:symbol" element={<PrivateRoute><CoinPage /></PrivateRoute>} />
-        <Route path="/ai-predictions" element={<PrivateRoute><AIPredictions /></PrivateRoute>} />
-        <Route path="/about" element={<PrivateRoute><About /></PrivateRoute>} />
-        <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
-        {/* Catch-all redirect to login */}
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-
+      {!hideNavbarAndFooter && <Navbar />}
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/coin/:symbol" element={<PrivateRoute><CoinPage /></PrivateRoute>} />
+          <Route path="/ai-predictions" element={<PrivateRoute><AIPredictions /></PrivateRoute>} />
+          <Route path="/about" element={<PrivateRoute><About /></PrivateRoute>} />
+          <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
+          {/* Catch-all redirect to login */}
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </main>
+      {!hideNavbarAndFooter && <Footer />}
     </>
   );
 }
 
+
 function App() {
   return (
     <Router>
-      <AppContent />
+      <div className="flex flex-col min-h-screen">
+        <AppContent />
+      </div>
     </Router>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full text-center py-4 border-t border-gray-300 dark:border-gray-700 text-sm text-gray-500 mt-8">
+      © 2025 ₿A$I. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
